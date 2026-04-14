d6e924234031ce9c590d70700e3d2a10bfa6bc82
Ana Clara Campanella Belchior <a.belchior@aluno.ifsp.edu.br>
2026-04-14 08:28:22 -0300

    produtos totalmente adicionados

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -13,15 +13,15 @@ export default function Produtos() {
             <div className='titulos'>
                 <h2>Produtos</h2>
                 <p>
-                    Conheça nossos produtos e escolha o que mais combina com você! 
-                    Perfeitos para acompanhar você em todas as ocasiões, 
+                    Conheça nossos produtos e escolha o que mais combina com você!
+                    Perfeitos para acompanhar você em todas as ocasiões,
                     nossos produtos são feitos com materiais de alta qualidade e design moderno.
                 </p>
 
                 <div className='produtos_grid'>
                     <div className='card'>
                         <div className='card-image'>
-                            <img src="{sandalia1}" alt="Sandália marrom" />
+                            <img src={sandalia1} alt="Sandália marrom" />
                             <span className='badge-venda'>Mais vendido</span>
                         </div>
                         <div className='card-info'>
@@ -30,6 +30,63 @@ export default function Produtos() {
                             <button className='btn'>Comprar agora</button>
                         </div>
                     </div>
+
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src={sandalia2} alt="Sandália pink" />
+                        </div>
+                        <div className='card-info'>
+                            <h3>Sandália Pink</h3>
+                            <p className='preco'>R$ 240,90</p>
+                            <button className='btn'>Comprar agora</button>
+                        </div>
+                    </div>
+
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src={tamanco} alt="Tamanco Marrom" />
+                        </div>
+                        <div className='card-info'>
+                            <h3>Tamanco Marrom</h3>
+                            <p className='preco'>R$ 120,00</p>
+                            <button className='btn'>Comprar agora</button>
+                        </div>
+                    </div>
+
+                     <div className='card'>
+                        <div className='card-image'>
+                            <img src={tenis} alt="Tênis preto" />
+                        </div>
+                        <div className='card-info'>
+                            <h3>Tênis Preto</h3>
+                            <p className='preco'>R$ 360,50</p>
+                            <button className='btn'>Comprar agora</button>
+                        </div>
+                    </div>
+
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src={bolsa} alt="Bolsa vermelha" />
+                        </div>
+                        <div className='card-info'>
+                            <h3>Bolsa Vermelha</h3>
+                            <p className='preco'>R$ 500,70</p>
+                            <button className='btn'>Comprar agora</button>
+                        </div>
+                    </div>
+
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src={note} alt="Porta notebook azul marinho" />
+                        </div>
+                        <div className='card-info'>
+                            <h3>Porta Notebook Azul Marinho</h3>
+                            <p className='preco'>R$ 170,99</p>
+                            <button className='btn'>Comprar agora</button>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </section>
